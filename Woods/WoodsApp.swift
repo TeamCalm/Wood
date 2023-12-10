f9a0d18dddc9f2778d05712cd52e45658a3dc7b7
Johannes <Hannes.9090@gmx.de>
2023-12-10 05:53:40 +0100

    Initial Commit

---
+++ Woods/WoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoodsApp.swift
+//  Woods
+//
+//  Created by Johannes Ahlborn on 10.12.23.
+//
+
+import SwiftUI
+
+@main
+struct WoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
